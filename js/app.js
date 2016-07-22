c18082375ed7ce51265647643ed6f2bca375d234
Mark Williams <github@shymedia.com>
2016-07-22 15:13:17 -0700

    New game working.

--- js/app.js
+++ js/app.js
@@ -11,7 +11,34 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-
+  	
+  	$("#userGuess").focus(function() {
+  		$(this).attr("placeholder", "");
+  	});
+  	$("#userGuess").blur(function() {
+  		$(this).attr("placeholder","Enter your Guess");
+  	});
+  	$(".new").click(function() {
+  		newGame();
+  	});
+  	
+	newGame();
 });
 
+var myNum = 0;
+var numGuesses = 0;
 
+function newGame () {
+	myNum = randomIntFromInterval(1,100);
+	numGuesses = 0;
+	console.log("new game: " + myNum);
+	$("#guessList").html("").hide();
+	$(".game > p").hide();
+}
+
+
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+    /* http://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript*/
+}
